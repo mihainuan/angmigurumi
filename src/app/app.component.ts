3514a214a4bcbc0e6550bbd3afda21284f179163
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-05 11:33:24 -0300

    first commit - Angmigurumi project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Amigurummi APP';
+  description = 'Amigurumi';
+  URL = 'http://euroroma.com.br/blog/wp-content/uploads/2020/09/amigurumi-macaco.png';
 }
