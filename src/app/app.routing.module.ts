9796c4c1bc270071a50c007821a7f37d260e183d
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-06 20:17:33 -0300

    logout Implementation

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { SignInComponent } from './home/signin/signin.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
@@ -9,7 +10,8 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 const routes: Routes = [
     { 
         path: '', 
-        component: SignInComponent
+        component: SignInComponent,
+        canActivate: [ AuthGuard ]
     },
     { 
         path: 'p/:userName', 
