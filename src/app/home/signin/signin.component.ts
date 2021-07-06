19013b9862bd1cdfb83c8bed6653e7fcc5b253f5
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-06 16:45:31 -0300

    ViewChild: obtendo referências do template

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
@@ -10,25 +10,23 @@ import { AuthService } from 'src/app/core/auth.service';
 export class SignInComponent implements OnInit{
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router
-        ){
-        
-    }
+    ){}
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
-
     }
 
     login() {        
-        console.log("Go To Authentication...");
+        console.log("Go-To -> Authentication...");
 
         const userName = this.loginForm.get('userName').value;
         const password = this.loginForm.get('password').value;
@@ -36,11 +34,12 @@ export class SignInComponent implements OnInit{
         this.authService
         .authenticate(userName,password)
         .subscribe( 
-            () => this.router.navigate(['p', userName]),
+            () => this.router.navigate(['p', userName]), // p/username
         err => {
-            console.log(err);
-            this.loginForm.reset();
-            alert(err.error.message);
+            this.loginForm.reset(); //Clear Form
+            this.userNameInput.nativeElement.focus(); //Element from DOM
+            console.log(err.error.message);
+            alert(err.error.message); //Msg on screen
         });
         
     }
