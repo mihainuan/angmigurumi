bab170235ba127d1d6af7054fe91b712f44e6410
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-06 18:44:57 -0300

    Token Local Storaging

---
+++ src/app/core/token/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'authToken';
+
+@Injectable({ providedIn: 'root'})
+export class TokenService {
+
+    hasToken(){
+        return !!this.getToken(); //If exists token, true if not false
+    }
+
+    setToken(token){
+        window.localStorage.setItem(KEY, token)
+    }
+
+    getToken(){
+        return window.localStorage.getItem(KEY);
+    }
+
+    trashToken(){
+        window.localStorage.removeItem(KEY);
+    }
+
+}
