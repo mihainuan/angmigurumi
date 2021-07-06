ee608b0d96d14a1f8b432a7729de5ffcdba1606a
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-06 19:23:17 -0300

    userService and Payload(data) from JWT Token (BehaviorSubject)

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000'
 @Injectable({
@@ -11,7 +12,7 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService
+    private userService: UserService
     ) { }
 
   authenticate(userName: string, password: string) {
@@ -24,7 +25,7 @@ export class AuthService {
     )
     .pipe(tap(res => {
       const authToken = res.headers.get('x-access-token');
-      this.tokenService.setToken(authToken);
+      this.userService.setToken(authToken);
       console.log(`Usuário ${userName.toUpperCase()} authenticated with TOKEN -> ${authToken}`);
     }))
   }
