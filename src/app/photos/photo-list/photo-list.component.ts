77a209db50872d08759f05e2891d23a53ecf9ad6
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-06 10:49:37 -0300

    Criada paginação e LoadButton Implemented

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
+import { concat, Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -17,9 +18,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   //Search string
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
   //Occurs after 1st instance in Angular 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];    
     this.debounce
     .pipe(debounceTime(420))
@@ -27,9 +32,23 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   }
 
   //Exclusive for Dependency Injection (Convention/Best Practices)
-  constructor(private activatedRoute: ActivatedRoute){}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService){}
+
+
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load(){
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+      if(!photos.length){
+       this.hasMore = false
+      }
+    });
   }
 
 }
