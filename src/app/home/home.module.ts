72319e21122081fdd8f2c63327f139d2e51372fe
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-09 15:07:04 -0300

    created "Register" page component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [ 
         ReactiveFormsModule,
         CommonModule,
         VmessageModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ]
 })
 export class HomeModule {
