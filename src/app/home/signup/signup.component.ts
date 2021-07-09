eb3b70cc842ab62eb4aaa2090f9cfb1a9a3783ba
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-09 15:30:46 -0300

    Criada validação baseada no modelo (Model-Driven Forms)

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
 
+    signupForm: FormGroup;
+
+    constructor (private formBuilder: FormBuilder){
+        this.ngOnInit();
+    }
+    
+    ngOnInit(): void {
+        this.signupForm = this.formBuilder.group({
+            emailAddress:['', 
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName:['', 
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName:['', 
+                [
+                    Validators.required,
+                    Validators.pattern(/^[a-z0-9_\-]+$/),
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ]
+            ],
+            password:['', 
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.maxLength(12)
+                ]
+            ],
+        })
+    }
 }
