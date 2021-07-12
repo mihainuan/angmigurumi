3dbf8b3e4ad5d4d6a748ef1cee40f5a389f04c2c
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-12 18:23:07 -0300

    ERRO RESOLVIDO: em: StaticInjectorError(AppModule) - Forum

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,47 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth.guard';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
-import { SignInComponent } from './home/signin/signin.component';
-import { SignUpComponent } from './home/signup/signup.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list-resolver';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
-
-//TODO: Organizando Rotas e Implementando Lazy Loading
 const routes: Routes = [
-    { 
-        path: '', 
-        component: HomeComponent,
-        canActivate: [ AuthGuard ],
-        children: [
-            {
-                path: '',
-                component: SignInComponent
-            },
-            {
-                path: 'signup',
-                component: SignUpComponent
-            }
-        ]
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
     },
-    { 
-        path: 'signup', 
-        component: SignUpComponent
+    {
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
     },
-    { 
-        path: 'p/:userName', 
-        component: PhotoListComponent, 
-        resolve: { photos: PhotoListResolver} 
+    {
+        path: 'p/:userName',
+        component: PhotoListComponent,
+        resolve: { photos: PhotoListResolver}
     },
-    { 
-        path: 'p/form', 
+    {
+        path: 'p/form',
         component: PhotoFormComponent
     },
-    { 
-        path: '**', 
-        component: NotFoundComponent 
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
 
@@ -50,6 +35,4 @@ const routes: Routes = [
     imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
     exports: [ RouterModule ]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
