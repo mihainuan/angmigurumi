5bef459516b47852e98ee346ec530a68ec1e1d08
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-14 17:01:14 -0300

    Adequando a exibição das fotos (FIXME)

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD_URL = 'http://localhost:3000/imgs/'
 @Component({
     selector: 'amg-photo',
     templateUrl: 'photo.component.html'
 })
 
 export class PhotoComponent {
+    private _url = '';
     @Input() title = '';
     @Input() description = '';
-    @Input() url = '';
+    @Input() set url(url: string) {
+        if(!url.startsWith('data')){
+            // this.url = CLOUD_URL + url; FIXME: Adequar carregamento de foto
+        }
+        this._url = url;
+    }
+    get url(){
+        return this._url;
+    }
 }
