49b0c2d4548591a66ebd8d3a9cf3b08050751653
Mihainuan de Sá Silva <mndv@gft.com>
2021-07-14 16:44:34 -0300

    Upload da foto

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -4,6 +4,7 @@ import { PhotoFormComponent } from './photo-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageComponent } from '../../shared/components/vmessage/vmessage.component';
 import { VmessageModule } from '../../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [ PhotoFormComponent ],
@@ -11,7 +12,8 @@ import { VmessageModule } from '../../shared/components/vmessage/vmessage.module
         CommonModule,
         ReactiveFormsModule,
         VmessageModule,
-        FormsModule
+        FormsModule,
+        RouterModule
     ]
 })
 export class PhotoFormModule {
