b5c3f0349d6b554fe1bc30f709d9e1a1b9d9de81
mihainuan <mihainuan@gmail.com>
2021-07-15 15:08:50 -0300

    Modelando dados do componente de notificação (AlertService)

--- src/app/photos/photo-details/photo-ownership/photo-ownership.directive.ts
+++ src/app/photos/photo-details/photo-ownership/photo-ownership.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit, Renderer, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { UserService } from 'src/app/core/user/user.service';
 import { Photo } from '../../photo/photo';
 
