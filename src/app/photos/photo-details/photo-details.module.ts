1f09181cb8a778a4bf49f984ac64bec209650cef
mihainuan <mihainuan@gmail.com>
2021-07-14 22:44:55 -0300

    FIXED: Adequar carregamento de foto

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { PhotoDetailsComponent } from "./photo-details.component";
-import { CommonModule } from "@angular/common";
-import { PhotoModule } from "../photo/photo.module";
+import { NgModule } from '@angular/core';
+import { PhotoDetailsComponent } from './photo-details.component';
+import { CommonModule } from '@angular/common';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
     declarations: [PhotoDetailsComponent],
     exports: [PhotoDetailsComponent],
     imports: [
-        CommonModule,    
+        CommonModule,
         PhotoModule
     ]
 })
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -7,27 +7,27 @@ const API = 'http://localhost:3000';
 @Injectable({providedIn: 'root'})
 export class PhotoService {
 
-    constructor (private http: HttpClient){}
-    
-    listFromUser(userName: string){
-        return this.http.get<Photo[]>(API + '/'+ userName +'/photos');
+    constructor (private http: HttpClient) {}
+
+    listFromUser(userName: string) {
+        return this.http.get<Photo[]>(API + '/' + userName + '/photos');
     }
 
-    listFromUserPaginated(userName: string, page: number){
+    listFromUserPaginated(userName: string, page: number) {
         const params = new HttpParams()
         .append('page', page.toString());
-        return this.http.get<Photo[]>(API + '/'+ userName +'/photos', { params });
+        return this.http.get<Photo[]>(API + '/' + userName + '/photos', { params });
     }
 
-    upload(description: string, allowComments: boolean, file: File){
+    upload(description: string, allowComments: boolean, file: File) {
         const formData = new FormData();
         formData.append('description', description);
         formData.append('allowComments', allowComments ? 'true' : 'false');
-        formData.append('imageFile', file);        
+        formData.append('imageFile', file);
         return this.http.post(API + '/photos/upload', formData);
     }
 
     findById(id: string) {
-        return this.http.get<Photo>(API+'/photos/'+id);
+        return this.http.get<Photo>(API + '/photos/' + id);
     }
 }
