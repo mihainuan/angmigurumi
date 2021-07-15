2a9b29628a6b06dd936f391c6a43569dfa5774a6
mihainuan <mihainuan@gmail.com>
2021-07-15 18:30:13 -0300

    O Serviço Title

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
@@ -8,7 +9,7 @@ import { PlatformDetectorService } from 'src/app/core/platform-detector/platform
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent implements OnInit, AfterViewInit{
+export class SignInComponent implements OnInit, AfterViewInit {
 
     loginForm: FormGroup;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
@@ -17,34 +18,38 @@ export class SignInComponent implements OnInit, AfterViewInit{
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService
-    ){}
+        private platformDetectorService: PlatformDetectorService,
+        private titleService: Title
+    ) {}
 
     ngOnInit(): void {
+        this.titleService.setTitle('Login - AngMigurumi');
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
     }
-    
+
     ngAfterViewInit(): void {
+        // tslint:disable-next-line: no-unused-expression
         this.platformDetectorService.isPlatformBrowser() &&
         this.userNameInput.nativeElement.focus();
     }
 
     login() {
         const userName = this.loginForm.get('userName').value;
-        const password = this.loginForm.get('password').value;        
+        const password = this.loginForm.get('password').value;
         this.authService
-        .authenticate(userName,password)
-        .subscribe( 
+        .authenticate(userName, password)
+        .subscribe(
             () => this.router.navigate(['user', userName]), // user/username
         err => {
-            this.loginForm.reset(); //Clear Form
-            this.platformDetectorService.isPlatformBrowser() && 
-                this.userNameInput.nativeElement.focus(); //Element from DOM
+            this.loginForm.reset(); // Clear Form
+            // tslint:disable-next-line: no-unused-expression
+            this.platformDetectorService.isPlatformBrowser() &&
+            this.userNameInput.nativeElement.focus(); // Element from DOM
             console.log(err.error.message);
-            alert(err.error.message); //Msg on screen
-        });        
+            alert(err.error.message); // Msg on screen
+        });
     }
 }
