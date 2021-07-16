f6c93ece582e0690cac1b0ea1e84a8736e5f0a80
mihainuan <mihainuan@gmail.com>
2021-07-16 16:50:55 -0300

    Stacktrace padronizada entre navegadores (StackTrace-js)

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,11 +1,19 @@
 import { ErrorHandler } from '@angular/core';
-
-
+import * as StackTrace from 'stacktrace-js';
 
 export class GlobalErrorHandler implements ErrorHandler{
 
     handleError(error: any): void {
         console.log('Passou pelo handleError!');
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                console.log(error);
+                const stackAsString = stackFrames
+                .map(sf => sf.toString())
+                .join('\n');
+            console.log(stackAsString);
+            });
         throw error;
     }
 }
